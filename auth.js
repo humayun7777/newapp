811fc5e12b222770bc152bd99b2149cabefbb08f
humayun7777 <130256323+humayun7777@users.noreply.github.com>
2024-04-18 11:51:35 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 const msalConfig = {
     auth: {
         clientId: "6cec9e80-48f0-4873-9776-076045267714",  // Your Application (client) ID from Azure AD B2C
-        authority: "https://heyhooley.b2clogin.com/heyhooley.onmicrosoft.com/B2C_1_signupsignin",  // Just the domain, tenant, and policy
+        authority: "https://heyhooley.b2clogin.com/heyhooley.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1_signupsignin",  // Just the domain, tenant, and policy
         redirectUri: "https://orange-sky-0e951470f.5.azurestaticapps.net"  // Ensure this matches one of your redirect URIs in Azure AD B2C configuration
     },
     cache: {
