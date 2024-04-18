6915dae4890dfee31ed0c83148489afa7b09211d
humayun7777 <130256323+humayun7777@users.noreply.github.com>
2024-04-17 19:19:41 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@ const msalConfig = {
     auth: {
         clientId: "ae6dbbe0-2a58-4202-8c6a-4505fe284ff4",  // Your Application (client) ID from Azure AD B2C
         authority: "https://heyhooley.b2clogin.com/heyhooley.onmicrosoft.com/B2C_1_signupsignin",  
-        redirectUri: "https://heyhooley.com"  // This must match the Azure AD B2C configuration exactly
+        redirectUri: "https://orange-sky-0e951470f.5.azurestaticapps.net"  // This must match the Azure AD B2C configuration exactly
     },
     cache: {
         cacheLocation: "sessionStorage",  // Recommended to keep cache in session storage for security reasons
