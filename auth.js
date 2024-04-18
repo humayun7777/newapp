575267bfc96609bd35e03a6c012a3db0385f6a3f
humayun7777 <130256323+humayun7777@users.noreply.github.com>
2024-04-18 14:02:18 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,15 +1,16 @@
-// Configuration for MSAL
 const msalConfig = {
     auth: {
-        clientId: "6cec9e80-48f0-4873-9776-076045267714",  // Your Application (client) ID from Azure AD B2C
-        authority: "https://heyhooley.b2clogin.com/heyhooley.onmicrosoft.com/B2C_1_signupsignin",  // Correct authority URL
-        redirectUri: "https://orange-sky-0e951470f.5.azurestaticapps.net"  // Ensure this matches one of your redirect URIs in Azure AD B2C configuration
+        clientId: "6cec9e80-48f0-4873-9776-076045267714",
+        authority: "https://heyhooley.b2clogin.com/heyhooley.onmicrosoft.com/B2C_1_signupsignin",
+        knownAuthorities: ["heyhooley.b2clogin.com"], // Add this line
+        redirectUri: "https://orange-sky-0e951470f.5.azurestaticapps.net"
     },
     cache: {
-        cacheLocation: "sessionStorage",  // Recommended to keep cache in session storage for security reasons
-        storeAuthStateInCookie: true  // Useful for maintaining state in browsers that block third-party cookies
+        cacheLocation: "sessionStorage",
+        storeAuthStateInCookie: true
     }
 };
+
 
 // Creating an instance of MSAL
 const msalInstance = new msal.PublicClientApplication(msalConfig);
